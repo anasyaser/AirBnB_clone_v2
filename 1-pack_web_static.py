ddab432e1fc6fbb3631cdc5de4b5031ed38b8117
anasyaser <anoos649@gmail.com>
2023-10-05 18:31:52 +0300

    Add static to archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Prepare current web static files and compress them"""
+from datetime import datetime
+from fabric.operations import local
+
+
+def do_pack():
+    """Compress files on local machine"""
+    local("mkdir -p versions")
+    archive_name = "web_static_{}.tgz"\
+    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
+    result = local("tar -cvzf versions/{} web_static".format(archive_name),
+                   capture=True)
+    if result.failed:
+        return
+    return "versions/{}".format(archive_name)

e8f2b1962841e6282a20bf1b3b9c635763b90a48
anasyaser <anoos649@gmail.com>
2023-10-06 04:06:33 +0300

    Task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Prepare current web static files, compress and deploy them them"""
+from datetime import datetime
+from fabric.operations import local, run, put
+import os
+
+
+env.hosts = ['54.197.110.58', '100.26.246.11']
+
+def do_pack():
+    """Compress files on local machine"""
+    local("mkdir -p versions")
+    archive_name = "web_static_{}.tgz"\
+    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
+    result = local("tar -cvzf versions/{} web_static".format(archive_name),
+                   capture=True)
+    if result.failed:
+        return
+    return "versions/{}".format(archive_name)
+
+def do_deploy(archive_path):
+    """Deploy Current version to all my servers"""
+    if not os.path.exist(archive_path):
+        return False
+
+    arch_name = archive_path.split('/')[-1]
+    arch_name_no_exten = arch_name.split('.')[1]
+    try:
+        put(archive_path, '/tmp/{}'.format(arch_name))
+        run('mkdir -p /data/web_static/releases/{}'.format(arch_name_no_exten))
+        run('tar -zxf /tmp/{} -C /data/web_staic/releases/{}'
+            .format(arch_name, arch_name_no_exten))
+        run('rm /tmp/{}'.format(arch_name))
+        run('mv /data/web_static/releases/{}/web_static/* /data\
+        /web_static/releases/{}/'.format(arch_name_no_exten, arch_name_no_exten))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(arch_name_no_exten))
+        run('rm -rf /data/web_static/current')
+        run('ls -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(arch_name_no_exten))
+    except Exception:
+        return False
+    return True
